1a30ad49748053abbc72f522d05917dc73932034
CaptainD1 <legohavend@gmail.com>
2020-09-24 15:22:34 -0400

    Made the menu modular

--- Assets/Scripts/Menus/ButtonHandler.cs
+++ Assets/Scripts/Menus/ButtonHandler.cs
@@ -1,15 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEditor; // For viewing all scenes. Might want to remove later
 using UnityEngine.SceneManagement;
+
+// TODO: Potentially split all of these into their own scripts for each menu
 
 public class ButtonHandler : MonoBehaviour {
 
     [SerializeField] private string START_SCENE = "main";
     [SerializeField] private string menuScene = "MainMenu";
     [SerializeField] private Canvas canvas = null;
+    [SerializeField] private GameObject creditsMenu = null;
+    [SerializeField] private GameObject mainMenu = null;
+    [SerializeField] private GameObject loadMenu = null;
+
+    private GameObject currentMenu = null;
+    private Stack<GameObject> previousMenus = new Stack<GameObject>();
+
     private string currentScene;
     private float lastTimeScale;
+
+    [SerializeField] private TMP_Dropdown loadDropdown = null;
+
+    void Awake() {
+        creditsMenu.SetActive(false);
+        loadMenu.SetActive(false);
+
+        currentMenu = mainMenu;
+    }
 
     void Start() {
         currentScene = SceneManager.GetActiveScene().name;
@@ -17,6 +37,14 @@ public class ButtonHandler : MonoBehaviour {
         if(canvas == null) {
             canvas = GetComponent<Canvas>();
         }
+
+        // Get all scenes and add to dropdown. May get removed later.
+        List<TMP_Dropdown.OptionData> optionDataList = new List<TMP_Dropdown.OptionData>();
+        foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+            if(scene.enabled) optionDataList.Add(new TMP_Dropdown.OptionData(scene.path));
+        }
+        loadDropdown.ClearOptions();
+        loadDropdown.AddOptions(optionDataList);
     }
 
     void Update() {
@@ -26,12 +54,12 @@ public class ButtonHandler : MonoBehaviour {
     }
 
     public void StartGame() {
+        // Start game if main menu
         if(currentScene == menuScene) {
             SceneManager.LoadScene(START_SCENE);
-        } else {
-            // Resume game
-            Time.timeScale = lastTimeScale;
+        } else { // Resume game otherwise
             canvas.enabled = false;
+            Time.timeScale = lastTimeScale;
         }
     }
 
@@ -39,6 +67,36 @@ public class ButtonHandler : MonoBehaviour {
         lastTimeScale = Time.timeScale;
         Time.timeScale = 0f;
         canvas.enabled = true;
-        // Pause
+    }
+
+    public void GoBack() {
+        if(previousMenus.Count > 0) {
+            currentMenu.SetActive(false);
+            currentMenu = previousMenus.Pop();
+            currentMenu.SetActive(true);
+        }
+    }
+
+    public void Exit() {
+        if(currentScene == menuScene) {
+            // We might not want to include this
+            Debug.Log("Exiting game!");
+            Application.Quit();
+        } else {
+            // Reset timescale
+            Time.timeScale = lastTimeScale;
+            SceneManager.LoadScene(menuScene);
+        }
+    }
+
+    public void OpenMenu(GameObject menu) {
+        currentMenu.SetActive(false);
+        previousMenus.Push(currentMenu);
+        currentMenu = menu;
+        currentMenu.SetActive(true);
+    }
+
+    public void Load() {
+        SceneManager.LoadScene(loadDropdown.captionText.text);
     }
 }
---
+++ Assets/Scripts/Menus/LoadCredits.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LoadCredits : MonoBehaviour {
+
+    [SerializeField] private TextMeshProUGUI textObj = null;
+    [SerializeField] private TextAsset textAsset = null;
+
+    void Start() {
+        textObj = GetComponent<TextMeshProUGUI>();
+        textObj.text = textAsset.text;
+    }
+}
