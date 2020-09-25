0dccd06f218f447ee05f9d7a6e21960e19888ff2
CaptainD1 <legohavend@gmail.com>
2020-09-24 20:07:01 -0400

    Added numerical resources and tweaked menu prefab

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -8,29 +8,24 @@ public class PlayerController : MonoBehaviour {
     private const int NUM_ROTATIONS = 8;
 
     // Declare components for caching
-    [SerializeField] private CharacterMovementController _movementController = null;
     [SerializeField] private Camera _mainCamera = null;
-
-    // Character attributes (TODO: Potentially split into a separate "CharacterController")
-    // It all really depends on how many components we actually want everything split into
-    [SerializeField] private float _movementSpeed = 15.0f;
-    [SerializeField] private int _spriteRotation = 0;
+    [SerializeField] private CharacterController _characterController = null;
 
     void Start() {
         // Override components if they haven't been set in the inspector
-        if (_movementController == null) _movementController = GetComponent<CharacterMovementController>();
         if (_mainCamera == null) _mainCamera = Camera.main;
+        if (_characterController == null) _characterController = GetComponent<CharacterController>();
     }
 
     void Update() {
         
         // Update character movement. I really don't like the non-raw input, it feels too sluggish
         Vector2 inputAxis = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        _movementController.Move(inputAxis.normalized * _movementSpeed);
+        _characterController.Move(inputAxis.normalized * _characterController.GetSpeed());
 
         // Update character rotation (angle of mouse relative to player)
         Vector2 rotation = _mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        _spriteRotation = (NUM_ROTATIONS + (int) Mathf.Round(Mathf.Atan2(rotation.y, rotation.x) /
-                (2*Mathf.PI) * NUM_ROTATIONS)) % NUM_ROTATIONS;
+        _characterController.SetSpriteRotation((NUM_ROTATIONS + (int) Mathf.Round(Mathf.Atan2(rotation.y, rotation.x) /
+                (2*Mathf.PI) * NUM_ROTATIONS)) % NUM_ROTATIONS);
     }
 }
