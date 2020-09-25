b639c66749c11259f455a06254af2b969cba0766
CaptainD1 <legohavend@gmail.com>
2020-09-25 10:21:07 -0400

    Added rough interaction system

---
+++ Assets/Scripts/Interactable/InteractableObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractableObject : MonoBehaviour {
+
+    [SerializeField] private string message;
+    [SerializeField] private UnityEvent interactAction;
+
+    public void Interact() {
+        interactAction?.Invoke();
+    }
+
+    public void DisplayTooltip() {
+        InputManager.instance.tooltip.gameObject.SetActive(true);
+        InputManager.instance.tooltip.SetText(message);
+        InputManager.instance.tooltip.transform.position = transform.position;
+    }
+}
