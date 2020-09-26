b0933e1c8f27a84bb1be0e5799328ac465edbb0f
juntaow0 <wang_j2@denison.edu>
2020-09-26 09:46:34 +0800

    Oscar's scripts

---
+++ Assets/Scripts/Oscar/playerDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerDeath : MonoBehaviour
+{
+   int health = 500;
+
+   private void OnCollisionEnter2D(Collision2D collision)
+   {
+      bool hitPlayer = collision.gameObject.tag == "Enemy";
+
+      if (hitPlayer)
+      {
+         health = health - 10;
+      }
+
+      if (health <= 0)
+      {
+         Destroy(gameObject);
+      }
+   }
+}
---
+++ Assets/Scripts/Oscar/playerMove.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+ private void OnMouseDrag()
+ {
+    Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    transform.position = new Vector3(newPos.x, newPos.y);
+}
+}
