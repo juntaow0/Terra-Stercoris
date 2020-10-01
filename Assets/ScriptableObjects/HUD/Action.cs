54c68a37d6144acea2250a4f6131ee75e24ffaf6
jeffstevens98 <43075360+jeffstevens98@users.noreply.github.com>
2020-10-01 18:28:20 -0400

    Some work done on the HUD action slots

---
+++ Assets/ScriptableObjects/HUD/Action.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newAction", menuName = "Action"]
+public class Action : ScriptableObject
+{
+    public new string name; //The the name of the action
+    public Sprite icon; //The icon that represents the action in the menu
+    public int strength; //How much damage/healing the ability does
+    public int powerCost; //How much power the action costs to be used
+    public int healthCost; //How much health the action costs to be used
+    public float cooldown; //The minimum time interval between uses of the ability
+}
--- Assets/Scripts/HUD/ActionSlot.cs
+++ Assets/Scripts/HUD/ActionSlot.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class ActionSlot : MonoBehaviour
@@ -8,7 +10,10 @@ public class ActionSlot : MonoBehaviour
  * 
  */ 
 {
-    //public List<actions> actions;
+    //make the action array a scriptable object
+    public Action wrench;
+    public String currentAction;
+    float cooldown;
 
     //1. Have a container full of possible items or abilities
     //2. Allow the assigning of one such item or ability to this action slot
@@ -19,17 +24,49 @@ public class ActionSlot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentAction = wrench.name; //for now, we start with the wrench action equipped by default
     }
 
     // Update is called once per frame
     void Update()
-    {   
+    {
+        if (cooldown > 0) //Cool down a use of an action
+        {
+            cooldown -= Time.deltaTime;
+            if (cooldown < 0)
+            {
+                cooldown = 0;
+            }
+        }
     }
 
-    /*
-    void changeAction(newAction)
+    void changeAction(String newAction)
     //Changes the current action available to use on the action slot
     {
+    
     }
-    */
+
+    public void useAction()
+    //Called by Input Manager to use the current slotted ability
+    {
+        if (cooldown == 0) //We can only use an action if it is not on cooldown
+        {
+            if (currentAction == wrench.name)
+            {
+                //call ability use from player gameobject here
+                cooldown = wrench.cooldown;
+                //call the NumericalResources class here to expend resources to use an ability
+            }
+            else if (currentAction == "sampleAction2")
+            {
+                //
+                ;
+            }
+        }
+        else //When we try to use an ability that is on cooldown, we trigger a sound and a visual effect
+        {
+            Debug.Log("That ability is on cooldown! You need to wait a little longer.")
+            //Note: Eventually add sound and a visual effect here
+        }
+    }
 }
