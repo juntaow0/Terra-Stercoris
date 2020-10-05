f852647b158febc13322276a4f87f960a855afd6
CaptainD1 <legohavend@gmail.com>
2020-10-05 11:09:00 -0400

    Fixed some issues from merge

---
+++ Assets/Scripts/Scene Management/TransitionController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionInterface : MonoBehaviour {
+    public void LoadScene(string scene) {
+        TransitionManager.instance.LoadScene(scene);
+    }
+}
