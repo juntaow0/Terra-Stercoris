ee5302639814309b0f27e09c927300d3d916651f
CaptainD1 <legohavend@gmail.com>
2020-10-18 14:56:11 -0400

    Added WeaponManager for persistent weapons

---
+++ Assets/Scripts/Combat/WeaponManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour {
+    
+    [SerializeField] private GameObject[] _weaponPrefabArray = null;
+    private Dictionary<int,GameObject> _weaponPrefabs;
+    public static WeaponManager instance {get; private set;}
+
+    void Awake() {
+        instance = this;
+
+        _weaponPrefabs = new Dictionary<int,GameObject>();
+        foreach(GameObject weapon in _weaponPrefabArray) {
+            int id = weapon.GetComponent<WeaponBehavior>().weaponStats.weaponID;
+            _weaponPrefabs[id] = weapon;
+        }
+    }
+
+    public GameObject CreateWeapon(Transform parent, int weaponID) {
+        if(_weaponPrefabs.ContainsKey(weaponID)) {
+            return Instantiate(_weaponPrefabs[weaponID], parent);
+        } else {
+            Debug.Log("No weapon with found with given ID!");
+            return null;
+        }
+    }
+}
