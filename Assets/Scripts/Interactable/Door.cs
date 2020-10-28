11faab5e674c162d7fe007961d07e44df406dee6
CaptainD1 <legohavend@gmail.com>
2020-10-28 11:08:53 -0400

    Added door scripts

---
+++ Assets/Scripts/Interactable/Door.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class Door : MonoBehaviour, IInteractable {
+
+    [SerializeField] private Sprite closedSprite;
+    [SerializeField] private Sprite openSprite;
+    [SerializeField] private bool startOpen = false;
+    private Collider2D collider;
+    private SpriteRenderer renderer;
+    private bool isOpen;
+    private string _message;
+    public string message {get {return _message;} set{_message = value;}}
+    public bool InteractEnabled {get {return true;} set {}}
+
+    void Start() {
+        collider = GetComponent<Collider2D>();
+        renderer = GetComponent<SpriteRenderer>();
+        if(startOpen) {
+            Open();
+        } else {
+            Close();
+        }
+    }
+
+    public void Interact() {
+        if(isOpen) {
+            Close();
+        } else {
+            Open();
+        }
+    }
+
+    public void StopInteract() {}
+
+    public void Close() {
+        isOpen = false;
+        renderer.sprite = closedSprite;
+        collider.isTrigger = false;
+        message = "Open Door";
+    }
+
+    public void Open() {
+        isOpen = true;
+        renderer.sprite = openSprite;
+        collider.isTrigger = true;
+        message = "Close Door";
+    }
+}
---
+++ Assets/Scripts/Interactable/SemiAutoDoor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class SemiAutoDoor : MonoBehaviour, IInteractable {
+
+    [SerializeField] private Sprite closedSprite;
+    [SerializeField] private Sprite openSprite;
+    private Collider2D collider;
+    private SpriteRenderer renderer;
+    private bool isOpen;
+    private string _message = "Open Door";
+    public string message {get {return _message;} set{_message = value;}}
+    public bool InteractEnabled {get {return !isOpen;} set {}}
+
+    void Start() {
+        collider = GetComponent<Collider2D>();
+        renderer = GetComponent<SpriteRenderer>();
+        Close();
+    }
+
+    public void Interact() {
+        Open();
+    }
+
+    public void StopInteract() {}
+
+    public void Close() {
+        isOpen = false;
+        renderer.sprite = closedSprite;
+        collider.isTrigger = false;
+    }
+
+    public void Open() {
+        isOpen = true;
+        renderer.sprite = openSprite;
+        collider.isTrigger = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        Close();
+    }
+}
