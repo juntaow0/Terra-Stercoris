d60f124662f81fb7fc97a101d0ca77fd98a260fc
juntaow0 <wang_j2@denison.edu>
2020-10-30 08:47:44 +0800

    manual ui fade

---
+++ Assets/Scripts/UI/FadeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeController : MonoBehaviour
+{
+    public void FadeToBlack() {
+        UIManager.instance.FadeIn(1f,null);
+    }
+
+    public void FadeFromBlack() {
+        UIManager.instance.FadeOut(1f, null);
+    }
+}
