8d89dbdd626751ed528fec8491df27821d96fc66
juntaow0 <wang_j2@denison.edu>
2020-11-13 07:48:38 +0800

    comm2

---
+++ Assets/Scripts/Scene Scripts/Comm2/Tuskcombat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Tuskcombat : MonoBehaviour
+{
+    [SerializeField] private UnityEvent OnDeath;
+    [SerializeField] private CharacterController TuskCC;
+    // Start is called before the first frame update
+    void Start() {
+        TuskCC = GetComponent<CharacterController>();
+        TuskCC.health.OnResourceUpdated += DeathEvent;
+    }
+
+    void DeathEvent(int health) {
+        if (health <= 0) {
+            OnDeath?.Invoke();
+            TuskCC.health.OnResourceUpdated -= DeathEvent;
+            enabled = false;
+        }
+    }
+}
