2884606a0dc99be70f468c64478825bc2cb5270e
CaptainD1 <legohavend@gmail.com>
2020-11-13 02:07:56 -0500

    Added beginning of basic inventory system

---
+++ Assets/Scripts/InventorySystem/InventoryController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour {
+
+    [SerializeField] int inventoryID;
+    public List<IItem> inventory {get {return InventoryManager.instance.GetItems(inventoryID);}}
+
+    void Start() {
+        InventoryManager.instance.Bind(inventoryID);
+    }
+
+    public void Take(IItem item) {
+        inventory.Add(item);
+    }
+
+    public void Drop(IItem item) {
+        inventory.Remove(item);
+    }
+
+    public void DropByName(string name) {
+        for(int i = 0; i < inventory.Count; ++i) {
+            if(inventory[i].ItemName == name) {
+                inventory.RemoveAt(i);
+                break;
+            }
+        }
+    }
+
+    public bool HasItem(IItem item) {
+        return inventory.Contains(item);
+    }
+
+    public bool HasItemByName(string name) {
+        foreach(IItem item in inventory) {
+            if(item.ItemName == name) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/InventorySystem/InventoryManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour {
+    
+    private Dictionary<int,List<IItem>> inventories;
+
+    public static InventoryManager instance {get; private set;}
+
+    void Awake() {
+        instance = this;
+
+        inventories = new Dictionary<int,List<IItem>>();
+    }
+
+    public void Bind(int id) {
+        if(!inventories.ContainsKey(id)) {
+            inventories[id] = new List<IItem>();
+        }
+    }
+
+    public List<IItem> GetItems(int id) {
+        return inventories[id];
+    }
+}
