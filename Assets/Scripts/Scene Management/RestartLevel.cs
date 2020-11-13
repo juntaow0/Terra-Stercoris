29f13f4b176e0f2389e77221c54412645c708bdb
juntaow0 <wang_j2@denison.edu>
2020-11-13 09:08:02 +0800

    Death screen and restart level

---
+++ Assets/Scripts/Scene Management/RestartLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+    private CharacterController playerCC;
+
+    private void Awake() {
+        playerCC = GetComponent<CharacterController>();
+        playerCC.OnDeath += Restart;
+    }
+
+    void Restart() {
+        playerCC.OnDeath -= Restart;
+        UIManager.instance.ShowDeathScreen();
+    }
+}
