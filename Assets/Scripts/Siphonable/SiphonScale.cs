a54dd074fae20eee893b8df2552da363d86b7e55
CaptainD1 <legohavend@gmail.com>
2020-11-12 21:48:41 -0500

    Added Lily Pad Puzzle to Comm1 Area and added more events to SiphonScale

--- Assets/Scripts/Siphonable/SiphonScale.cs
+++ Assets/Scripts/Siphonable/SiphonScale.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SiphonScale : MonoBehaviour, ISiphonable {
 
@@ -8,7 +9,25 @@ public class SiphonScale : MonoBehaviour, ISiphonable {
     [SerializeField] float minScale;
     [SerializeField] float scalePerHealth;
 
-    float scale {get {return transform.localScale.x;} set {transform.localScale = new Vector2(value, value);}}
+    [SerializeField] UnityEvent onMaxScale;
+    [SerializeField] UnityEvent onMinScale;
+    [SerializeField] UnityEvent leaveMaxScale;
+    [SerializeField] UnityEvent leaveMinScale;
+
+    float scale {get {return transform.localScale.x;} set {
+        if(value >= maxScale) {
+            value = maxScale;
+            onMaxScale?.Invoke();
+        } else if(value <= minScale) {
+            value = minScale;
+            onMinScale?.Invoke();
+        } else if(scale == minScale) {
+            leaveMinScale?.Invoke();
+        } else if(scale == maxScale) {
+            leaveMaxScale?.Invoke();
+        }
+        transform.localScale = new Vector2(value, value);
+    }}
 
     public bool IsSiphonable {get {return scale > minScale;} set {}}
     public bool IsShareable {get {return scale < maxScale;} set{}}
