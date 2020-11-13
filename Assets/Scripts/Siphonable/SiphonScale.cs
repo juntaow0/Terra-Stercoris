034493faa29fe6d079df8878715cf1ab1a45ce7b
CaptainD1 <legohavend@gmail.com>
2020-11-12 22:48:30 -0500

    Added smooth scaling on SiphonScale

--- Assets/Scripts/Siphonable/SiphonScale.cs
+++ Assets/Scripts/Siphonable/SiphonScale.cs
@@ -8,13 +8,33 @@ public class SiphonScale : MonoBehaviour, ISiphonable {
     [SerializeField] float maxScale;
     [SerializeField] float minScale;
     [SerializeField] float scalePerHealth;
+    [SerializeField] float scaleSpeed = 3.0f;
 
     [SerializeField] UnityEvent onMaxScale;
     [SerializeField] UnityEvent onMinScale;
     [SerializeField] UnityEvent leaveMaxScale;
     [SerializeField] UnityEvent leaveMinScale;
 
-    float scale {get {return transform.localScale.x;} set {
+    float _scale;
+    float visualScale;
+
+    void Start() {
+        scale = visualScale = transform.localScale.x;
+    }
+
+    void Update() {
+        if(scale != visualScale) {
+            float difference = (scale - visualScale) * scaleSpeed;
+            if(Mathf.Abs(difference) / scale > 0.01) {
+                visualScale = visualScale + difference * Time.deltaTime;
+            } else {
+                visualScale = scale;
+            }
+            transform.localScale = new Vector2(visualScale, visualScale);
+        }
+    }
+
+    float scale {get {return _scale;} set {
         if(value >= maxScale) {
             value = maxScale;
             onMaxScale?.Invoke();
@@ -26,7 +46,7 @@ public class SiphonScale : MonoBehaviour, ISiphonable {
         } else if(scale == maxScale) {
             leaveMaxScale?.Invoke();
         }
-        transform.localScale = new Vector2(value, value);
+        _scale = value;
     }}
 
     public bool IsSiphonable {get {return scale > minScale;} set {}}
