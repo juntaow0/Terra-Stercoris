ce78cadfb50419b71310717147b6ed4ace25e5a2
juntaow0 <wang_j2@denison.edu>
2020-11-18 21:30:43 +0800

    finishing up

---
+++ Assets/Scripts/Logistics/ChoiceTrackerForEvent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoiceTrackerForEvent : MonoBehaviour
+{
+    public string key;
+    public void Track(int choice) {
+        ChoiceTracker.Track(key, choice);
+    }
+}
---
+++ Assets/Scripts/Scene Scripts/ResetRotation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetRotation : MonoBehaviour
+{
+    public void StandUp() {
+        transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
+    }
+}
