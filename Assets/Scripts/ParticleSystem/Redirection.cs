fdf8efaadf524bd398074e17e6d7eacb1c401d53
juntaow0 <wang_j2@denison.edu>
2020-11-19 10:45:42 +0800

    pre-release build test

---
+++ Assets/Scripts/ParticleSystem/Redirection.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Redirection : MonoBehaviour
+{
+    private TransitionController tc;
+    private void Awake() {
+        tc = GetComponent<TransitionController>();
+    }
+    public void TrailerButton() {
+        float value = Random.value;
+        if (value > 0.5f) {
+            tc.LoadSceneByName("IntroOld");
+        } else {
+            Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        }
+    }
+}
